8b3a66d6193cce1f03c42c829f3f066a4b0c7eb4
Olivér Mrakovics <oliver.mrakovics@gmail.com>
2023-09-20 18:37:04 +0200

    fix(schemas/label): optimize the minimum char length

--- src/lib/schemas/label.ts
+++ src/lib/schemas/label.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const labelSchema = z.object({
   title: z
     .string()
-    .min(3, { message: "The title must be at least 3 characters" })
+    .min(1, { message: "The title must be at least 1 characters" })
     .max(32, { message: "The title can be maximum 20 characters" }),
   color: z.string(),
 });
