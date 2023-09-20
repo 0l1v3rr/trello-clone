504943fa1d27b6361676c0c9fe12e42f1c4f5964
Olivér Mrakovics <oliver.mrakovics@gmail.com>
2023-09-20 18:33:33 +0200

    chore: update to nextjs 13.5

--- src/app/(dashboard)/[username]/[boardSlug]/@card/(.)cards/[cardId]/_components/dialog-wrapper.tsx
+++ src/app/(dashboard)/[username]/[boardSlug]/@card/(.)cards/[cardId]/_components/dialog-wrapper.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { FC, PropsWithChildren } from "react";
+import { useRouter } from "next/navigation";
+import { useCardContext } from "@/context/card-context";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 interface DialogWrapperProps extends PropsWithChildren {}
 
 const DialogWrapper: FC<DialogWrapperProps> = ({ children }) => {
+  const router = useRouter();
+  const { board } = useCardContext();
+
   return (
-    <Dialog open>
+    <Dialog
+      open
+      onOpenChange={(open) => {
+        if (open) {
+          router.push(`/${board.owner.username}/${board.slug}`);
+          router.refresh();
+        }
+      }}
+    >
       <DialogContent showCloseBtn={false}>{children}</DialogContent>
     </Dialog>
   );
